dcdb9b7165cd4a25d1f24015979b620180bc32ef
LISX579 <76584182+LISX579@users.noreply.github.com>
2021-03-20 22:08:09 +0800

    Vuejs

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        //默认
+        // '@':src
+        'assets': '/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
+}
